9bc8f8689e0589c6d20dd37857e9955517986cb8
Yusuf Can <ycan@THB-DEV-279.local>
2020-02-10 15:13:25 +0300

    file structure

---
+++ example-application/app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = 3000;
+server.listen(port);
